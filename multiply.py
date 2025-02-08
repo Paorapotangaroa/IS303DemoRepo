8ca999ca4402182b7971be375c528f2509cef90b
Paorapotangaroa <92904680+Paorapotangaroa@users.noreply.github.com>
2025-02-07 23:08:11 -0700

    Create multiplyTwoNumbers function
    Wrote a function that accepts two numbers and multiplies

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Toa Pita
+# Section 42
+# 2/7/2025
+# Description: Accept two numbers and return the product
+def multiplyTwoNumbers(iNum1,iNum2):
+    # Multiply and return the result
+    return iNum1*iNum2
+
