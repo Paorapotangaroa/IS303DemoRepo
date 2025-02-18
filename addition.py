81223ebbb18ca90b8e4f52d4a9f610157aa2bcad
Paorapotangaroa <92904680+Paorapotangaroa@users.noreply.github.com>
2025-02-17 19:27:34 -0700

    adding addition and do the math

---
+++ addition.py
@@ -0,0 +1,7 @@
+# Toa Pita
+# Section 42
+# 2/17/2025
+# Description: Accept two numbers and return the sum
+def addTwoNumbers(iNum1,iNum2):
+    # Multiply and return the result
+    return iNum1+iNum2
