6245e9e6633c5f73ae7e26e0775143ccb3303783
Paorapotangaroa <92904680+Paorapotangaroa@users.noreply.github.com>
2025-02-17 19:34:56 -0700

    wrote do the math

--- doTheMath.py
+++ doTheMath.py
@@ -4,3 +4,13 @@
 # Description: Take input for two numbers. 
 # Check if the sum of those numbers is the same as their product
 
+from addition import addTwoNumbers
+from multiply import multiplyTwoNumbers
+
+iFirstNum = int(input("Please enter a whole number: "))
+iSecondNum = int(input("Please enter another whole number: "))
+
+if(addTwoNumbers(iFirstNum,iSecondNum)==multiplyTwoNumbers(iFirstNum,iSecondNum)):
+    print("The sum and product are the same")
+else:
+    print("The sum and product are different")
